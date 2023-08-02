d80d237bf11edc96511e529efd2404abc4d20054
enzo filippo <enzo.filippo23@gmail.com>
2023-08-02 15:47:36 +1200

    added animation on first load

--- src/_components/navigation.tsx
+++ src/_components/navigation.tsx
@@ -49,13 +49,13 @@ const navigationLinks: NavigationLinks[] = [
 ];
 
 export function MobileNav() {
-  return <div className="flex sm:hidden">mobile</div>;
+  return <div className="fixed w-full flex z-20 sm:hidden">mobile</div>;
 }
 export function DesktopNav() {
   const { pathname } = useLocation();
 
   return (
-    <div className="fixed h-screen left-5 top-0 hidden sm:flex sm:items-center lg:left-10">
+    <div className="fixed h-screen left-5 top-0 z-20 hidden sm:flex sm:items-center lg:left-10">
       <div className="flex flex-col gap-3">
         {navigationLinks.map((link) => {
           const { active, icon, name, route } = link;
@@ -80,7 +80,7 @@ export function DesktopNav() {
                   src={onRoute ? active : icon}
                 />
                 <p
-                  className={`hidden px-1 text-lg font-inter font-bold capitalize bg-gradient-to-b from-neutral-50 to-neutral-50/30 bg-clip-text text-transparent overflow-hidden whitespace-nowrap ${
+                  className={`hidden px-1 text-lg font-orbitron font-bold capitalize bg-gradient-to-b from-neutral-50 to-neutral-50/30 bg-clip-text text-transparent overflow-hidden whitespace-nowrap ${
                     !onRoute && "group-hover:flex"
                   }`}
                 >
@@ -94,7 +94,7 @@ export function DesktopNav() {
         <div className="group border rounded-full border-white/10 bg-neutral-900 w-fit p-2">
           <div className="flex gap-2 items-center group-hover:animate-widthGrow">
             <img className="h-10 w-10" src={ResumeIcon} />
-            <p className="hidden px-1 text-lg font-inter font-bold capitalize bg-gradient-to-b from-neutral-50 to-neutral-50/30 bg-clip-text text-transparent group-hover:flex overflow-hidden whitespace-nowrap">
+            <p className="hidden px-1 text-lg font-orbitron font-bold capitalize bg-gradient-to-b from-neutral-50 to-neutral-50/30 bg-clip-text text-transparent group-hover:flex overflow-hidden whitespace-nowrap">
               my resume
             </p>
           </div>
--- src/_routes/home.tsx
+++ src/_routes/home.tsx
@@ -1,3 +1,38 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-  return <div>home</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-6 h-full py-2 sm:gap-12 sm:py-4 sm:pl-[4vw]">
+      {/* Hero */}
+      <div className="flex flex-col items-center sm:gap-2 lg:gap-4">
+        <h3 className="max-w-sm sm:max-w-3xl lg:max-w-4xl flex py-3 text-2xl sm:text-5xl lg:text-6xl leading-[1.1] text-center font-nunito font-semibold tracking-tight bg-gradient-to-b from-white to-white/60 bg-clip-text text-transparent">
+          Enter a world where creativity merges with technology
+        </h3>
+
+        <p className="text-sm sm:text-xl lg:text-2xl max-w-sm sm:max-w-lg font-mono font-medium text-neutral-400 text-center tracking-tight sm:leading-6">
+          Meet{" "}
+          <a
+            href="https://www.linkedin.com/in/enzo-filippo-31559bab/"
+            className="text-neutral-100 underline"
+            target="_blank"
+          >
+            Enzo Filippo
+          </a>
+          , web developer bringing your online presence to life
+        </p>
+      </div>
+
+      {/* Links */}
+
+      <div className="flex gap-4">
+        <Link to="/projects" className="border rounded-3xl px-4 py-2">
+          Browse projects
+        </Link>
+
+        <Link to="/contact" className="border rounded-3xl px-4 py-2">
+          Contact me
+        </Link>
+      </div>
+    </div>
+  );
 }
